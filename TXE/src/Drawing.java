17e2f7c67f548763ad9e6fe95c794ae586447c84
Great Ark Studios <ericz2241@gmail.com>
2013-08-23 10:11:00 -0400

    Added Drawing Pad

---
+++ TXE/src/Drawing.java
@@ -0,0 +1,130 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Event;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+import javax.swing.*;
+    
+public class Drawing {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		JToolBar Tb = new JToolBar();
+		JScrollBar Sb = new JScrollBar();
+		frame.setVisible(true);
+		
+		JRadioButton blue = new JRadioButton("Blue");
+		JRadioButton red = new JRadioButton("Red");
+		JRadioButton green = new JRadioButton("Green");
+		JRadioButton black = new JRadioButton("Black");
+		JRadioButton gray = new JRadioButton("Gray");
+		final DrawPad drawPad = new DrawPad();
+
+		JButton clearButton = new JButton("Clear");
+		clearButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				drawPad.clear();
+			}
+		});
+		blue.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				drawPad.graphics2D.setPaint(Color.BLUE);
+			}
+		});
+		red.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				drawPad.graphics2D.setPaint(Color.RED);
+			}
+		});
+		green.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				drawPad.graphics2D.setPaint(Color.GREEN);
+			}
+		});
+		black.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				drawPad.graphics2D.setPaint(Color.BLACK);
+			}
+		});
+		gray.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				drawPad.graphics2D.setPaint(Color.GRAY);
+			}
+		});
+		frame.add(clearButton, BorderLayout.SOUTH);
+		Tb.add(blue);
+		Tb.add(red);
+		Tb.add(green);
+		Tb.add(black);
+		Tb.add(gray);
+		JScrollPane Sp = new JScrollPane(drawPad,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		frame.add(Sp, BorderLayout.CENTER);
+
+		frame.add(Tb, BorderLayout.NORTH);
+		frame.setMaximumSize(new Dimension(1920, 1080));
+		frame.setSize(500, 500);
+		frame.setMinimumSize(new Dimension(400,400));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+	}
+
+}
+
+class DrawPad extends JComponent {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	Image image;
+	Graphics2D graphics2D;
+	int currentX, currentY, oldX, oldY;
+
+	public DrawPad() {
+
+		setDoubleBuffered(false);
+		addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent Me) {
+				oldX = Me.getX();
+				oldY = Me.getY();
+			}
+		});
+		addMouseMotionListener(new MouseMotionAdapter() {
+			public void mouseDragged(MouseEvent e) {
+				currentX = e.getX();
+				currentY = e.getY();
+				if (graphics2D != null)
+					graphics2D.drawLine(oldX, oldY, currentX, currentY);
+				repaint();
+				oldX = currentX;
+				oldY = currentY;
+			}
+		});
+	}
+
+	public void paintComponent(Graphics g) {
+		if (image == null) {
+			image = createImage(1920, 1080);
+			graphics2D = (Graphics2D) image.getGraphics();
+			graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+					RenderingHints.VALUE_ANTIALIAS_ON);
+			clear();
+		}
+		g.drawImage(image, 0, 0, null);
+	}
+
+	public void clear() {
+		graphics2D.setPaint(Color.white);
+		graphics2D.fillRect(0, 0, 1920, 1080);
+		graphics2D.setPaint(Color.black);
+		repaint();
+	}
+}
