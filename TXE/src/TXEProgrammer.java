bb9be47eaf83480b289e47a8b545c3adfacc25a8
Great Ark Studios <ericz2241@gmail.com>
2014-01-26 19:24:47 -0500

    Auto complete made with the fife auto complete library

--- TXE/src/TXEProgrammer.java
+++ TXE/src/TXEProgrammer.java
@@ -67,12 +67,17 @@ import javax.swing.tree.TreePath;
 import javax.swing.undo.CannotRedoException;
 import javax.swing.undo.UndoManager;
 
-import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
-
 import com.sun.speech.freetts.Voice;
 import com.sun.speech.freetts.VoiceManager;
 
 import say.swing.JFontChooser;
+
+import org.fife.ui.autocomplete.AutoCompletion;
+import org.fife.ui.autocomplete.BasicCompletion;
+import org.fife.ui.autocomplete.CompletionProvider;
+import org.fife.ui.autocomplete.DefaultCompletionProvider;
+import org.fife.ui.rsyntaxtextarea.*;
+import org.fife.ui.autocomplete.*;
 
 /**
  * 
@@ -177,11 +182,15 @@ public class TXEProgrammer extends JFrame {
 		TXEAREA.setCodeFoldingEnabled(true);
 		TXEAREA.setEOLMarkersVisible(false);
 		TXEAREA.setAutoIndentEnabled(true);
-
+		CompletionProvider provider = createCompletionProvider();
+		AutoCompletion ac = new AutoCompletion(provider);
+		ac.install(TXEAREA);
 		findText.setToolTipText("Type word or phrase to be found");
 		findText.setSize(50, 200);
 		findText.setText("Type word or phrase to be found here.");
 
+		  
+		
 		this.getContentPane().add(splitpane);
 
 		ImageIcon ImgIc = new ImageIcon(getClass().getResource(
@@ -1793,7 +1802,71 @@ public class TXEProgrammer extends JFrame {
 		}
 
 	}
+    
+		private CompletionProvider createCompletionProvider() {
 
+		      // A DefaultCompletionProvider is the simplest concrete implementation
+		      // of CompletionProvider. This provider has no understanding of
+		      // language semantics. It simply checks the text entered up to the
+		      // caret position for a match against known completions. This is all
+		      // that is needed in the majority of cases.
+		      DefaultCompletionProvider provider = new DefaultCompletionProvider();
+
+		      // Add completions for all Java keywords. A BasicCompletion is just
+		      // a straightforward word completion.
+		      provider.addCompletion(new BasicCompletion(provider, "abstract"));
+		      provider.addCompletion(new BasicCompletion(provider, "assert"));
+		      provider.addCompletion(new BasicCompletion(provider, "break"));
+		      provider.addCompletion(new BasicCompletion(provider, "case"));
+		      provider.addCompletion(new BasicCompletion(provider, "catch"));
+		      provider.addCompletion(new BasicCompletion(provider, "class"));
+		      provider.addCompletion(new BasicCompletion(provider, "const"));
+		      provider.addCompletion(new BasicCompletion(provider, "continue"));
+		      provider.addCompletion(new BasicCompletion(provider, "default"));
+		      provider.addCompletion(new BasicCompletion(provider, "do"));
+		      provider.addCompletion(new BasicCompletion(provider, "else"));
+		      provider.addCompletion(new BasicCompletion(provider, "enum"));
+		      provider.addCompletion(new BasicCompletion(provider, "extends"));
+		      provider.addCompletion(new BasicCompletion(provider, "final"));
+		      provider.addCompletion(new BasicCompletion(provider, "finally"));
+		      provider.addCompletion(new BasicCompletion(provider, "for"));
+		      provider.addCompletion(new BasicCompletion(provider, "goto"));
+		      provider.addCompletion(new BasicCompletion(provider, "if"));
+		      provider.addCompletion(new BasicCompletion(provider, "implements"));
+		      provider.addCompletion(new BasicCompletion(provider, "import"));
+		      provider.addCompletion(new BasicCompletion(provider, "instanceof"));
+		      provider.addCompletion(new BasicCompletion(provider, "interface"));
+		      provider.addCompletion(new BasicCompletion(provider, "native"));
+		      provider.addCompletion(new BasicCompletion(provider, "new"));
+		      provider.addCompletion(new BasicCompletion(provider, "package"));
+		      provider.addCompletion(new BasicCompletion(provider, "private"));
+		      provider.addCompletion(new BasicCompletion(provider, "protected"));
+		      provider.addCompletion(new BasicCompletion(provider, "public"));
+		      provider.addCompletion(new BasicCompletion(provider, "return"));
+		      provider.addCompletion(new BasicCompletion(provider, "static"));
+		      provider.addCompletion(new BasicCompletion(provider, "strictfp"));
+		      provider.addCompletion(new BasicCompletion(provider, "super"));
+		      provider.addCompletion(new BasicCompletion(provider, "switch"));
+		      provider.addCompletion(new BasicCompletion(provider, "synchronized"));
+		      provider.addCompletion(new BasicCompletion(provider, "this"));
+		      provider.addCompletion(new BasicCompletion(provider, "throw"));
+		      provider.addCompletion(new BasicCompletion(provider, "throws"));
+		      provider.addCompletion(new BasicCompletion(provider, "transient"));
+		      provider.addCompletion(new BasicCompletion(provider, "try"));
+		      provider.addCompletion(new BasicCompletion(provider, "void"));
+		      provider.addCompletion(new BasicCompletion(provider, "volatile"));
+		      provider.addCompletion(new BasicCompletion(provider, "while"));
+
+		      // Add a couple of "shorthand" completions. These completions don't
+		      // require the input text to be the same thing as the replacement text.
+		      provider.addCompletion(new ShorthandCompletion(provider, "sysout",
+		            "System.out.println(", "System.out.println("));
+		      provider.addCompletion(new ShorthandCompletion(provider, "syserr",
+		            "System.err.println(", "System.err.println("));
+
+		      return provider;
+
+		}
 	public static void main(String[] args) {
 		TXEProgrammer txe1 = new TXEProgrammer();
 
