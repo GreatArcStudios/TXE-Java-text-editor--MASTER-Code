d7e56e789dec419a32231d75a0cdc61b3535d0e6
Great Ark Studios <ericz2241@gmail.com>
2014-02-06 20:48:23 -0500

    prep work for custom files

---
+++ TXE/src/txe.java
@@ -0,0 +1,16 @@
+
+public class txe implements java.io.Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public String fileName;
+	public String text;
+	public String color;
+	public String size;
+	public String style;
+	public String editable;
+	public String alignment;
+	
+}
